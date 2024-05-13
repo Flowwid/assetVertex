d58b8c4f1d5049c736524acc811c7dc8b2dcba5f
[Hilmy-19] <[hilmyaiman625@gmail.com]>
2024-05-13 10:29:25 +0700

    pembagian route

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//DASHBOARD
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//AUTHENTICATION
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //DIVISION
+
+    //BUDGETING
+
+    //EVENT
+
+    //ASSET-CATEGORY
+
+    //ASSET-LIST
+
+    //ASSET-BOM
+
+    //MAINTENANCE
 });
 
 require __DIR__.'/auth.php';
