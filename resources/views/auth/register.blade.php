cb9bc722ef8d001e6ae48588c6c748803695474f
deni151515 <denifajar15@gmail.com>
2024-06-02 17:08:55 +0700

    tampilan login dan register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,120 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+    <div class="bg">  
+        <div class="bg2" >
+            <div class="left">
+                <div class="logo">
+                    <img src="./images/logo_white.png"  alt="">
+                </div>
+                <div class="title1">
+                <img src="./images/icons/vector1.png" style="height: 25px; width: 25px;" alt="">
+                </div>
+                <div class="title2">
+                    <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.</p>
+                </div>
+                <div class="title3">
+                    <p>Vincent Obi</p>
+                </div>
+                <div class="title4">
+                <img src="./images/icons/vector2.png" style="height: 25px; width: 25px;" alt="">
+                </div>
+            </div>
+            <div class="right">
+                <div class="header-top">
+                    <a href="javascript:window.history.back();" class="back-link"> <  Back</a>
+                    <h1 class="welcome-text">Welcome</h1>
+                </div>
+                <div class="header"> 
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <h2>Login To Your Account!</h2>
+                    <p>Fill the information below to get back into your asset management.</p>
+                </div> 
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <!-- Name -->
+                    <div class="form-group">
+                        <x-input-label for="name" :value="__('Name')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Enter your Name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <!-- Position -->
+                    <div class="form-group">
+                        <x-input-label for="position" :value="__('position')" />
+                        <x-text-input id="position" class="block mt-1 w-full" type="text" name="position" :value="old('position')" required autofocus autocomplete="position" placeholder="Enter your Position"/>
+                        <x-input-error :messages="$errors->get('position')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="form-group">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="Enter your Email"/>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="form-group">
+                        <x-input-label for="password" :value="__('Password')" />
+                        <div class="password-toggle">
+                            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" placeholder="Enter your Password"/>
+                            <span class="toggle-password" onclick="togglePassword(this)">Show</span>
+                        </div>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="form-group">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <div class="password-toggle">
+                            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm your Password"/>
+                            <span class="toggle-password" onclick="togglePassword(this)">Show</span>
+                        </div>
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="checkbox-group">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <label for="remember_me">I agree to terms & conditions</label>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="forgot-password">
+                            <x-primary-button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                {{ __('Register') }}
+                            </x-primary-button>
+                            <a class="text-blue-500 hover:text-blue-700" href="{{ route('login') }}">
+                                {{ __('Already Have Acoount?') }}
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Position -->
-        <div class="mt-4">
-            <x-input-label for="position" :value="__('position')" />
-            <x-text-input id="position" class="block mt-1 w-full" type="text" name="position" :value="old('position')" required autofocus autocomplete="position" />
-            <x-input-error :messages="$errors->get('position')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
+
+<script>
+    function togglePassword(element) {
+        var passwordField = element.previousElementSibling;
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+           
+        } else {
+            passwordField.type = "password";
+          
+        }
+    }
+</script>
+
+<style>
+    .password-toggle {
+        position: relative;
+    }
+    .toggle-password {
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+        cursor: pointer;
+        color: #000;
+    }
+</style>
