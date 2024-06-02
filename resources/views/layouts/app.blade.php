1f6ace0d9dcd0a27d6b668956151609d29d66157
Muhammad Haikal <muhaikael@gmail.com>
2024-06-02 18:17:34 +0800

    Tab icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>Assets Vertex</title>
 
+        <link rel="icon" href="./images/logo_blue.png" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Asset Vertex</title>
+    <link rel="icon" href="./images/logo_blue.png" type="image/x-icon">
     <link rel="stylesheet" href="./css/landingStyle.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
