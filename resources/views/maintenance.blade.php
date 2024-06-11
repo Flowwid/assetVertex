0f386ef60cc17d528aafb92151ae97540aac7352
[Hilmy-19] <[hilmyaiman625@gmail.com]>
2024-06-11 22:48:28 +0700

    maintenance

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -15,7 +15,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editDataModalLabel">Add Data</h5>
+                <h5 class="modal-title" id="editDataModalLabel">Add Datas</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
